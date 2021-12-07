d92e145aefc64f6a79b67a3c38d1fd7b4defdf72
Annisa Fahira <annisa.fahira999@gmail.com>
2021-12-06 16:27:29 -0800

    fixed whiteboard color picker and point size

---
+++ templates/static/canvas.js
@@ -0,0 +1,66 @@
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');;
+const width = canvas.width = window.innerWidth-50;
+const height = canvas.height = window.innerHeight-50;
+
+
+
+ctx.fillStyle = 'rgb(242, 242, 242)';
+ctx.fillRect(0,0,width,height);
+
+
+function pick(e){
+    color = this.value;
+    ctx.globalCompositeOperation = 'source-over';
+}
+function size(e){
+    thickness = this.value;
+}
+document.getElementById("colorPicker").onchange = pick;
+document.getElementById("pointSize").onchange = size;
+
+/*function eraser(){
+    ctx.globalCompositeOperation = "destination-out";  
+    ctx.strokeStyle = "rgb(242, 242, 242)";
+}*/
+
+$(document).ready(function() {
+    var flag, dot_flag = false,
+      prevX, prevY, currX, currY = 0
+    var $canvas = $('#canvas');
+    var ctx = $canvas[0].getContext('2d');
+    
+  
+    $canvas.on('mousemove mousedown mouseup mouseout', function(e) {
+      prevX = currX;
+      prevY = currY;
+      currX = e.clientX - $canvas.offset().left;
+      currY = e.clientY - $canvas.offset().top;
+      if (e.type == 'mousedown') {
+        flag = true;
+      }
+      if (e.type == 'mouseup' || e.type == 'mouseout') {
+        flag = false;
+      }
+      if (e.type == 'mousemove') {
+        if (flag) {
+          ctx.beginPath();
+          ctx.moveTo(prevX, prevY);
+          ctx.lineTo(currX, currY);
+          ctx.strokeStyle = color;
+          ctx.lineWidth = thickness;
+          ctx.stroke();
+          ctx.closePath();
+        }
+      }
+    });
+
+    $('.clear').on('click', function(e) {
+        c_width = $canvas.width();
+        c_height = $canvas.height();
+        ctx.fillStyle = 'rgb(242, 242, 242)';
+        ctx.clearRect(0, 0, c_width, c_height);
+        ctx.fillRect(0, 0, c_width, c_height);
+      });
+  });
+  

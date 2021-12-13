c187437a1ae9d6f5954d41e826f2f6dac44157fc
Renee <reneehelfert1998@gmail.com>
2021-12-13 02:42:29 -0500

    Added Profile UI

--- templates/static/canvas.js
+++ templates/static/canvas.js
@@ -107,3 +107,11 @@ $(document).ready(function() {
       });
   });
 
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "25%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
